0062990060dc59963d03872611ce7577f33b4ae8
uzorsamuel <samuzor7.com>
2024-12-04 05:05:03 +0900

    uzor added dictionary

---
+++ fhf.py
@@ -0,0 +1,15 @@
+igbo_dictionary = {
+    'nkita' : "dog" ,
+    'oche': "chair",
+    'nma': "knife",
+    'nti': "ear",
+    'bia': "come",
+    'je': "go",
+    'ego': "money",
+    'efere': "plate",
+    'chineke': "Jesus",
+    'biko': "please",
+}
+
+word = input('enter an igbo word: ')
+print(igbo_dictionary[word])
